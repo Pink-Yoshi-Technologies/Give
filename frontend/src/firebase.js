410e1b8c81756ec0441e90a2f56dd0c2296676a2
jaydelal13 <jlal056@aucklanduni.ac.nz>
2025-08-22 12:25:59 +1200

    fixed bug in the firebase file where authorisation was not being set

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
